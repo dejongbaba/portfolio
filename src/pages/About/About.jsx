04a41793dc34d75654688b738a829b8a9d290854
Adedeji Agunbiade <ade@kobo360.com>
2021-02-18 10:51:33 +0100

    added fixed to mobile view

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -13,72 +13,15 @@ function About() {
       </p>
       <h4 className="about__text_motto">
         <span className="gradientFade">Think.</span>
-        <span className="gradientFade delay1s">Make.</span>{' '}
-        <span className="gradientFade delay2s">Solve</span>
+        <span className="gradientFade delay1s">Create.</span>{' '}
+        <span className="gradientFade delay2s">Solve.</span>
       </h4>
-      <a href="#contact" className="about__button button__green">
+      <a
+        href="#contact"
+        className="about__button fixed__button button__green"
+      >
         Contact Me
       </a>
-
-      {/*<OwlCarousel*/}
-      {/*  className="about__subsection"*/}
-      {/*  nav*/}
-      {/*  items={3}*/}
-      {/*  margin={10}*/}
-      {/*>*/}
-      {/*  <div className="detail__item">*/}
-      {/*    <div className="detail__preview">*/}
-      {/*      <img*/}
-      {/*        src={require('../../assets/images/banner-five.jpg')}*/}
-      {/*        alt=""*/}
-      {/*      />*/}
-      {/*    </div>*/}
-      {/*    <p className="detail__category">User Interface</p>*/}
-      {/*    <p className="detail__text">*/}
-      {/*      Lorem ipsum dolor sit amet, consectetur adipisicing elit.*/}
-      {/*      Adipisci aperiam, aspernatur atque commodi cum deserunt*/}
-      {/*      dolorem eos ex fugit id impedit, iure maxime molestiae*/}
-      {/*      numquam officia placeat, tempora tenetur velit?*/}
-      {/*    </p>*/}
-      {/*    <button className="button button__deep_blue">*/}
-      {/*      Show Me*/}
-      {/*    </button>*/}
-      {/*  </div>*/}
-      {/*  <div className="detail__item">*/}
-      {/*    <div className="detail__preview">*/}
-      {/*      <img*/}
-      {/*        src={require('../../assets/images/banner-one.jpg')}*/}
-      {/*        alt=""*/}
-      {/*      />*/}
-      {/*    </div>*/}
-      {/*    <p className="detail__category">User Interface</p>*/}
-      {/*    <p className="detail__text">*/}
-      {/*      Lorem ipsum dolor sit amet, consectetur adipisicing elit.*/}
-      {/*      Adipisci aperiam, aspernatur atque commodi cum deserunt*/}
-      {/*      dolorem eos ex fugit id impedit, iure maxime molestiae*/}
-      {/*      numquam officia placeat, tempora tenetur velit?*/}
-      {/*    </p>*/}
-      {/*    <button className="button button__green">Show Me</button>*/}
-      {/*  </div>*/}
-      {/*  <div className="detail__item">*/}
-      {/*    <div className="detail__preview">*/}
-      {/*      <img*/}
-      {/*        src={require('../../assets/images/banner-one.jpg')}*/}
-      {/*        alt=""*/}
-      {/*      />*/}
-      {/*    </div>*/}
-      {/*    <p className="detail__category">User Interface</p>*/}
-      {/*    <p className="detail__text">*/}
-      {/*      Lorem ipsum dolor sit amet, consectetur adipisicing elit.*/}
-      {/*      Adipisci aperiam, aspernatur atque commodi cum deserunt*/}
-      {/*      dolorem eos ex fugit id impedit, iure maxime molestiae*/}
-      {/*      numquam officia placeat, tempora tenetur velit?*/}
-      {/*    </p>*/}
-      {/*    <button className="button button__deep_blue">*/}
-      {/*      Show Me*/}
-      {/*    </button>*/}
-      {/*  </div>*/}
-      {/*</OwlCarousel>*/}
     </div>
   );
 }
